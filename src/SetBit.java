57bd5ef9f63d058fad7b511a369151bed91784e6
sagul7 <sagulhameed339@gmail.com>
2023-07-18 12:51:56 +0530

    Initial commit

---
+++ src/SetBit.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SetBit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextByte();
+        int B = sc.nextInt();
+        if(A==B)
+        {
+            System.out.println(caculatePow(A));
+        }
+        else
+        System.out.println(caculatePow(A)+caculatePow(B));
+    }
+    public static int caculatePow(int A)
+    {
+        if(A==0)
+            return 1;
+        return 2 * caculatePow(A-1);
+    }
+}
